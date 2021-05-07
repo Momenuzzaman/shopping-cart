f63635e87dafdea1daed7c08bee297cd14178939
mdemon420 <mdeamon4@gmail.com>
2021-05-08 01:39:26 +0600

    second commit

--- shopping-cart/index.js
+++ shopping-cart/index.js
@@ -1,24 +1,50 @@
 
-
 document.querySelector("#case-increase").addEventListener("click",function(){
-   const caseInput = document.querySelector("#case-count");
-   const caseCount = parseInt(caseInput.value);
-   const caseNewCount = caseCount + 1;
-   caseInput.value= caseNewCount;  
- 
-   const caseTotal = caseNewCount * 59;
-   document.querySelector("#caseTotal").innerText ="$"+caseTotal;
- 
+   handelProduct(true);
 
 });
 document.querySelector("#case-decrease").addEventListener("click", function(){
+   handelProduct(false);
+   
+});
+
+
+function handelProduct(isInCrease){
    const caseInput = document.querySelector("#case-count");
    const caseCount = parseInt(caseInput.value);
-   const caseNewCount = caseCount - 1;
+   let caseNewCount = caseCount;
+   if(isInCrease == true){
+      caseNewCount = caseCount + 1;
+   }
+   else if(isInCrease ==  false && caseCount > 0){
+      caseNewCount = caseCount -1 ;
+   }
    caseInput.value = caseNewCount;
 
    const caseTotal = caseNewCount * 59;
    document.querySelector("#caseTotal").innerText = "$" + caseTotal;
 
+}
+
+// document.querySelector("#case-increase").addEventListener("click",function(){
+//    const caseInput = document.querySelector("#case-count");
+//    const caseCount = parseInt(caseInput.value);
+//    const caseNewCount = caseCount + 1;
+//    caseInput.value= caseNewCount;  
+ 
+//    const caseTotal = caseNewCount * 59;
+//    document.querySelector("#caseTotal").innerText ="$"+caseTotal;
+ 
+
+// });
+// document.querySelector("#case-decrease").addEventListener("click", function(){
+//    const caseInput = document.querySelector("#case-count");
+//    const caseCount = parseInt(caseInput.value);
+//    const caseNewCount = caseCount - 1;
+//    caseInput.value = caseNewCount;
+
+//    const caseTotal = caseNewCount * 59;
+//    document.querySelector("#caseTotal").innerText = "$" + caseTotal;
+
    
-});
+// });
